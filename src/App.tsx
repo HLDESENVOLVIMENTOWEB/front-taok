04f79715a4d7cd13bfc535333102cc24b0b096a7
Hyuri <hyuri.miranda@sambatech.com.br>
2025-01-24 12:32:15 -0300

    fix: colocar front para

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
-import Dashboard from './pages/Dashboard.tsx';
+
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <h1>as</h1>
   );
 };
 
